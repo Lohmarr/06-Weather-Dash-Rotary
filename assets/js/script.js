d992abc674589c66966a899ff60f72887c6fb226
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-03-27 15:05:07 -0400

    Fixed search history malfunctions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@ const searchHistory = document.querySelector('#search-history');
 const apiKey = '8815fa9153dba675fe64e2b5f006967b';
 
 // City User Input
-searchForm.addEventListener('submit', (event) => {
+function search() {
   event.preventDefault();
 
-  let city = cityInput.value.trim();
+  let city = cityInput.value.toUpperCase().trim();
 
   if (city === '') {
     alert('Please enter a city name');
@@ -22,22 +22,21 @@ searchForm.addEventListener('submit', (event) => {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-
       displayCurrentWeather(data.list[0]);
 
       displayForecast(data.list.filter((data, index) => index % 8 === 0));
     })
     .catch(error => console.error(error));
+}
+
+searchForm.addEventListener('submit', (event) => {
+  search()
 });
 
 // display current weather
 function displayCurrentWeather(weatherData) {
   let cityName = cityInput.value.toUpperCase()
 
-  cityInput.value = '';
-
-  console.log(weatherData)
   let html = `
     <h2>${cityName} (${weatherData.dt_txt}) <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png" alt="${weatherData.weather[0].description}"></h2>
     <p>Temperature: ${weatherData.main.temp} °F</p>
@@ -66,21 +65,40 @@ function displayForecast(forecastData) {
 
 // add city to search history
 function addToSearchHistory(city) {
-  let html = `<li><button>${city}</button></li>`;
-  searchHistory.innerHTML += html;
+  let searchStorage = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-  let lastCityButton = searchHistory.lastElementChild.querySelector('button');
-  lastCityButton.addEventListener('click', () => {
-    // simulate form submission with city name
-    cityInput.value = lastCityButton.textContent;
-    searchForm.dispatchEvent(new Event('submit'));
+  if (!searchStorage.includes(city)) {
+    searchStorage.push(city.toUpperCase());
+    localStorage.setItem('searchHistory', JSON.stringify(searchStorage));
+
+    let html = `<li class='city-button'><button>${city}</button></li>`;
+    searchHistory.innerHTML += html;
+  }
+
+  let cityButtons = document.querySelectorAll('.city-button');
+
+  cityButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      cityInput.value = button.textContent;
+      search()
+    });
   });
-
-  // save search history to local storage
-  localStorage.setItem('searchHistory', searchHistory.innerHTML);
 }
 
 // load search history from local storage
 if (localStorage.getItem('searchHistory')) {
-  searchHistory.innerHTML = localStorage.getItem('searchHistory');
-}
+  let searchStorage = JSON.parse(localStorage.getItem('searchHistory')) || []
+
+  searchStorage.forEach(city => {
+    let html = `<li class='city-button'><button>${city}</button></li>`;
+    searchHistory.innerHTML += html;
+  })
+
+  let cityButtons = document.querySelectorAll('.city-button');
+
+  cityButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      cityInput.value = button.textContent;
+      search()
+    });
+})}
