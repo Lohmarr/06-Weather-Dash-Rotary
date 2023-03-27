631c6c31097f881d47d951cb8fb42060047464e4
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-03-27 13:32:31 -0400

    Fixed 5-day forecast to be proper to day

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,44 +4,43 @@ const currentWeather = document.querySelector('#current-weather');
 const forecast = document.querySelector('#forecast');
 const searchHistory = document.querySelector('#search-history');
 
-let apiKey = '8815fa9153dba675fe64e2b5f006967b';
+const apiKey = '8815fa9153dba675fe64e2b5f006967b';
 
-// handle form submission
+// City User Input
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
+
   let city = cityInput.value.trim();
 
-  // check if city is empty
   if (city === '') {
     alert('Please enter a city name');
     return;
   }
 
-  // clear search input
-  cityInput.value = '';
-
-  // add city to search history
   addToSearchHistory(city);
 
-  // make API request
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-      // display current weather
+
       displayCurrentWeather(data.list[0]);
 
-      // display 5-day forecast
-      displayForecast(data.list.slice(1, 6));
+      displayForecast(data.list.filter((data, index) => index % 8 === 0));
     })
     .catch(error => console.error(error));
 });
 
 // display current weather
 function displayCurrentWeather(weatherData) {
+  let cityName = cityInput.value.toUpperCase()
+
+  cityInput.value = '';
+
+  console.log(weatherData)
   let html = `
-    <h2>${weatherData.name} (${new Date(weatherData.dt * 1000).toLocaleDateString()}) <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png" alt="${weatherData.weather[0].description}"></h2>
-    <p>Temperature: ${weatherData.main.temp} &#8451;</p>
+    <h2>${cityName} (${weatherData.dt_txt}) <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png" alt="${weatherData.weather[0].description}"></h2>
+    <p>Temperature: ${weatherData.main.temp} °F</p>
     <p>Humidity: ${weatherData.main.humidity}%</p>
     <p>Wind Speed: ${weatherData.wind.speed} m/s</p>
   `;
@@ -54,8 +53,8 @@ function displayForecast(forecastData) {
   forecastData.forEach(data => {
     html += `
       <div>
-        <p>Date: ${new Date(data.dt * 1000).toLocaleDateString()}</p>
-        <p>Temperature: ${data.main.temp} &#8451;</p>
+        <p>Date: ${data.dt_txt}</p>
+        <p>Temperature: ${data.main.temp} °F</p>
         <p>Humidity: ${data.main.humidity}%</p>
         <p>Wind Speed: ${data.wind.speed} m/s</p>
         <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}">
@@ -70,7 +69,6 @@ function addToSearchHistory(city) {
   let html = `<li><button>${city}</button></li>`;
   searchHistory.innerHTML += html;
 
-  // add event listener to search history button
   let lastCityButton = searchHistory.lastElementChild.querySelector('button');
   lastCityButton.addEventListener('click', () => {
     // simulate form submission with city name
